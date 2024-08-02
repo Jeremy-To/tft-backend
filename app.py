0d79b119cd2f079bf3ce148b23770565295b4efe
Jérémy TO <jeremy_to@outlook.fr>
2024-08-02 11:09:42 +0200

    fix cors

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import malware
 import forensic
 import io
 import os
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
